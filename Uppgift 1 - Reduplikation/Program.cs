4566f5e058ff9db2953ce5995a4918ff4f7b8a19
KungeniDjunglen <111962656+Dungeater1337@users.noreply.github.com>
2023-11-24 09:30:37 +0100

    Klar
    Klar

---
+++ Uppgift 1 - Reduplikation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+class Progam
+{
+    static void Main()
+    {
+        Console.WriteLine("Ordet?");
+        string Text = Console.ReadLine();
+        int Lenght = Text.Length;
+
+        Console.WriteLine("Upprepningar ?");
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 0; i < n; i++)
+        {
+
+            if(Text.Length > 10)
+            {
+                Console.WriteLine("Kan inte ha mer än nio bokstäver i ordet");
+                break;
+            }
+
+    
+            if(n > 9)
+            {
+                Console.WriteLine("får inte upprepas mer än nio gånger");
+                break;
+            }
+
+            Console.Write(Text);
+        }
+    }
+}
