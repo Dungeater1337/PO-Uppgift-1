1fe662781ad3df06ea724915ea52655e4a5f3006
KungeniDjunglen <111962656+Dungeater1337@users.noreply.github.com>
2023-11-24 10:44:46 +0100

    dsdsd
    sdsdsd

---
+++ Uppgift 2 - Tre i Rad/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("N ? ");
+        int N = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (int a = 1; a < N; a++)
+        {
+            //(a, a + 1, a + 2)
+            int produkt = a * (a + 1) * (a + 2);
+
+            if (produkt < N)
+            {
+                Console.WriteLine(produkt);
+                count++;
+            }
+            else
+            {
+                break; 
+            }
+        }
+
+        Console.WriteLine($"Svar: {count}");
+    }
+}
+
+
+
